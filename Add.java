819e294813ab9cc9eba71aa82df79827e1aa2e4a
MShiri9392 <shiri957313@gmail.com>
2026-05-23 11:22:55 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class project {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+    }
+}
